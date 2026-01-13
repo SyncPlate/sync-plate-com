48b0cce834c33e5555306d8b74084cc50529336d
Keegan Bruer <keegan.bruer@gmail.com>
2026-01-13 04:42:43 -0500

    Landing Splash

--- app/page.tsx
+++ app/page.tsx
@@ -1,27 +1,84 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
+  const items = [
+    { label: "Home", href:"/" },
+    { label: "Features", href:"/" },
+    { label: "Screenshots", href:"/screenshots" },
+    { label: "Pricing", href:"/" },
+    { label: "FAQ", href:"/" },
+    { label: "Contact", href:"/" },
+  ]
   return (
-    <div className="flex min-h-screen items-center justify-center bg-[#ECFFEF] font-sans">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center gap-5 py-20 px-16 bg-[#8CE299]">
-        <div style={{width:100, height:100}}>
+    <div>
+      <div style={{overflow:"hidden", borderRadius:40, position:"relative", height: 800, margin:10}}>
+        <div style={{
+          background: "linear-gradient(-103deg,transparent 40%, rgba(140, 226, 153, 1) 100%)",
+          position: "absolute", top: 0, left: 0, width: "50%", bottom:0
+        }}></div>
+        <div style={{
+          background: "linear-gradient(103deg,transparent 40%, rgba(140, 226, 153, 1) 100%)",
+          position: "absolute", top: 0, right: 0, width: "50%", bottom:0
+        }}></div>
+        <div style={{
+          position: "absolute", top: 0, left: 0,
+          display: "flex", flexDirection: "row", padding: 20, alignItems: "center", gap: 10, fontSize: 20, color: "white",
+
+        }}>
           <Image
-            className=""
-            src="/icon-transparent.png"
-            alt="Next.js logo"
-            width={200}
-            height={200}
-            priority
-            style={{minWidth:200, width:200, minHeight:200, height:200}}
+              className="dark:invert"
+              src="/web-app-manifest-512x512.png"
+              alt="Next.js logo"
+              width={50}
+              height={50}
+              priority
+              style={{minWidth:50, width:50, minHeight:50, height:50}}
           />
+          
+          <span style={{
+            textShadow:"0px 5px 5px black", fontWeight:"bold"
+          }}>SyncPlate</span>
         </div>
-        <h1 className="text-shadow-lg max-w-md text-center text-5xl font-semibold leading-10 tracking-tight text-white">
-          Sync Plate
-        </h1>
-        <h2 className="text-shadow-lg max-w-md text-center text-2xl font-semibold leading-10 tracking-tight text-white">
-          Plates, People, Plans
-        </h2>
         
+        <div style={{
+          position: "absolute", top: 0, left: 0, right: 0, bottom: 0,
+          display: "flex", flexDirection: "column", alignItems: "center", justifyContent:"center",
+          gap: 10, fontSize: 20, color: "black",
+          height:90, paddingTop:200
+        }}>
+          <h1 style={{ fontSize: 40, color: "white", textShadow: "0px 3px 15px black", fontWeight: "bold" }}>
+            Making Cooking Collaborative
+          </h1>
+          <p style={{ maxWidth: 500, textAlign:"center" }}>
+            SyncPlate helps you gather and share recipes, plan daily and holiday dinners, and keeps everyone on the same plate 😊
+          </p>
+        </div>
+        <Image
+          src="/app-tri-demo.png"
+          alt="Next.js logo"
+          width={600}
+          height={377}
+          priority
+          style={{position:"absolute", bottom:0, left:"50%", translate:"-50% 0"}}
+        />
+          
+        <div style={{
+          position: "absolute", top: 0, left: "20%", right: "20%",
+          display: "flex", flexDirection: "row", padding: 20, alignItems: "center", justifyContent:"center",
+          gap: 10, fontSize: 20, color: "black",
+          height:90
+        }}>
+          {items.map((item) => (
+            <Link key={item.label} href={item.href} style={{flex:1, height:"100%", display: "flex", justifyContent:"center", alignItems:"center"}}>
+                {item.label}
+            </Link>
+          ))}
+          
+        </div>
+      </div>
+      <main style={{minHeight:500}}>
+
       </main>
     </div>
   );
--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   basePath: '',
+  async headers() {
+    return [
+      {
+        source: '/.well-known/apple-app-site-association',
+        headers: [
+          {
+            key: 'Content-Type',
+            value: 'application/json',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
