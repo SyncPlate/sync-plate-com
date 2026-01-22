de37a6d65a6ab939382d26f2df687737d64426a8
Keegan Bruer <keegan.bruer@gmail.com>
2026-01-22 18:33:27 -0500

    test mailing list

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,9 +1,14 @@
 // import Image from "next/image";
 // import Link from "next/link";
 
+import { Suspense } from "react";
 import { RecipeInfo } from "./recipeinfo";
 
 
 export default function Home() {
-  return <RecipeInfo />
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <RecipeInfo />
+    </Suspense>
+  );
 }
