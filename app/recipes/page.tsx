ee4dc2724098aa480f938606319775d30de197d7
Keegan Bruer <keegan.bruer@gmail.com>
2026-01-22 18:25:51 -0500

    test mailing list

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -1,11 +1,14 @@
 // import Image from "next/image";
 // import Link from "next/link";
 
-export default function Home() {
+import { useRouter } from "next/router";
 
+export default function Home() {
+  const router = useRouter();
+  const { id } = router.query;
   return (
-    <div>
-      hello
+    <div style={{color:"black"}}>
+      recipe ID { id}
     </div>
   );
 }
