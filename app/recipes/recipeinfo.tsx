04d36b4f8cf60e201b981915fe462060d1537986
Keegan Bruer <keegan.bruer@gmail.com>
2026-01-27 13:29:09 -0500

    get recipe info

--- app/recipes/recipeinfo.tsx
+++ app/recipes/recipeinfo.tsx
@@ -1,10 +1,23 @@
 "use client"
 
 import { useSearchParams } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export function RecipeInfo() {
     const searchParams = useSearchParams();
     const id = searchParams.get('id');
+    const [recipe, setRecipe] = useState()
+    useEffect(() => {
+        const getRecipe = async () => {
+            const res = await fetch(`https://api.sync-plate.com/v1.0.0/api/recipes/${id}`);
+            const recipe = await res.json();
+            console.log("recipe", recipe)
+            setRecipe(recipe)
+        }
+        getRecipe()
+        const interval = setInterval(getRecipe, 30 * 1000);
+        return () => clearInterval(interval);
+    }, [])
     return (
         <div style={{color:"black"}}>
         recipe ID {id}
